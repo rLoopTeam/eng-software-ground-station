7dac0b698bf6277bebb8bb4d0ff764edbe82470d
Piense <piense@gmail.com>
2016-07-17 21:10:30 -0700

    Add material-ui
    Note that you have to remove the version of react that's in the
    material-ui folder.

--- telemetry-ui/src/RawScreen.jsx
+++ telemetry-ui/src/RawScreen.jsx
@@ -2,6 +2,17 @@ import leftPad from 'left-pad';
 import rightPad from 'right-pad';
 import React from 'react';
 import _ from 'underscore';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import Toggle from 'material-ui/Toggle';
+
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 import parameters from '../parameters.json'
 
@@ -37,16 +48,34 @@ var NodeTable = ({params, values}) =>
 
 var RawScreen = ({latestValues}) => {
   return (
-    <div style={{padding: 30}}>
-      {_.map(parameters, (params, nodeName) => {
-        return (
-          <div key={nodeName}>
-            <h1>{nodeName} node:</h1>
-            <NodeTable params={params} values={latestValues[nodeName]} />
-          </div>
-        );
-      })}
-    </div>
+	<div style={{padding: 30}}>
+	  {_.map(parameters, (params, nodeName) => {
+		return (
+			<MuiThemeProvider>
+			<Paper>
+			<div key={nodeName}>
+
+				<Paper style={{padding: 2, marginBottom: 10, marginTop: 10}}>
+				<h1>{nodeName} node:</h1>
+				</Paper>
+
+				<div>
+					<RaisedButton label="Flash Teensy" primary={true} style={{margin: 10}}/>
+					<RaisedButton label="Update Pi" primary={true} style={{margin: 10}}/>
+					<RaisedButton label="Rename Node" primary={true} style={{margin: 10}}/>
+				</div>
+
+				<div style={{width: 150}}>
+						<Toggle label="Logging" style={{margin: 10}} />
+				</div>
+				
+				<NodeTable params={params} values={latestValues[nodeName]} />
+		  </div>
+		  </Paper>
+		  </MuiThemeProvider>
+		);
+	  })}
+	</div>
   );
 };
 
