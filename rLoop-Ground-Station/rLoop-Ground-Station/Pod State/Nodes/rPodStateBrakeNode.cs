e521fefe3a1e0cd7cd5c7b76bd7754cd9d9cf0e2
Tyler Lindell <tylerlindell@gmail.com>
2016-11-14 12:53:23 -0800

    Adding communication and update for EddyBrakes in TEST tab

--- rLoop-Ground-Station/rLoop-Ground-Station/Pod State/Nodes/rPodStateBrakeNode.cs
+++ rLoop-Ground-Station/rLoop-Ground-Station/Pod State/Nodes/rPodStateBrakeNode.cs
@@ -8,8 +8,37 @@ namespace rLoop_Ground_Station.Pod_State.Nodes
 {
     public class rPodStateBrakeNode : rPodStateNodeStateI
     {
+        public float EBLeftLimitSwitch;
+        public float EBRightLimitSwitch;
+        public float EBLeftMLP; // lblLeftMLP
+        public float EBRightMLP; //lblRightMLP
+
+        public rPodStateBrakeNode()
+        {
+        }
+
+        //Should always line up with: http://confluence.rloop.org/display/SD/FCU+Node+Telemetry
         public override void ProcessParameter(List<DataParameter> parameterList)
         {
+            foreach (DataParameter p in parameterList)
+            {
+                if (p.Index == 0 && p.Data is float)
+                {
+                    EBLeftLimitSwitch = (float)p.Data;
+                }
+                if (p.Index == 1 && p.Data is float)
+                {
+                    EBRightLimitSwitch = (float)p.Data;
+                }
+                if (p.Index == 2 && p.Data is float)
+                {
+                    EBLeftMLP = (float)p.Data;
+                }
+                if (p.Index == 3 && p.Data is float)
+                {
+                    EBRightMLP = (float)p.Data;
+                }
+            }
         }
     }
 }
